240b56c9b79bbe654999b5c1c3862027d05ae1d4
mmsalvador <msalvador.matheus@gmail.com>
2024-07-10 22:18:47 -0300

    renamed:    my-first-project-java/src/MyClass.java -> my-first-project-java/src/stdy/msalvador/firstweek/MyClass.java renamed:    my-first-project-java/README.md -> my-first-project-java/src/stdy/msalvador/firstweek/README.md

--- my-first-project-java/src/stdy/msalvador/firstweek/MyClass.java
+++ my-first-project-java/src/stdy/msalvador/firstweek/MyClass.java
@@ -1,10 +1,11 @@
+package stdy.msalvador.firstweek;
 public class MyClass {
     public static String insertName (String firstName, String lastName){
 
         return firstName.concat(" ").concat(lastName);
     }
 
-
+    //**Sempre respeitar identação */
     public static void main(String[] args){
         String firstName = "Matheus";
         String lastName = "Salvador";
